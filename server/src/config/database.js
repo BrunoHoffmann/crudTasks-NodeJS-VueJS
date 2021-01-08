58f8375b3a109d443a3b6ea3d1e1a4b8bf8ab475
Bruno Hoffmann <brunohoffmanndev@gmail.com>
2021-01-07 22:23:47 -0300

    first commit

---
+++ server/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '5477',
+  database: 'tasks',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
